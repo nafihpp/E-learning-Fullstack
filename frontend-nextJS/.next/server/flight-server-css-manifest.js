6dec3421006c64b51eff9cccd5297c086d86ff43
nafihpp <nafihpp@gmail.com>
2023-11-29 23:12:57 +0400

    initial commit

---
+++ frontend-nextJS/.next/server/flight-server-css-manifest.js
@@ -0,0 +1,12 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/Users/mohammednafih/Desktop/E-learning full-stack/frontend-nextJS/app/layout.tsx": [
+      "/Users/mohammednafih/Desktop/E-learning full-stack/frontend-nextJS/app/globals.css"
+    ]
+  },
+  "cssModules": {
+    "/Users/mohammednafih/Desktop/E-learning full-stack/frontend-nextJS/app/page": [
+      "/Users/mohammednafih/Desktop/E-learning full-stack/frontend-nextJS/app/globals.css"
+    ]
+  }
+}
